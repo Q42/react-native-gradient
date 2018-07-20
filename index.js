b672541649e197122ec69c45a2488ed0adb1b3a9
Jasper Haggenburg <jhaggenburg@me.com>
2018-07-20 15:49:46 +0200

    Initial setup for iOS

---
+++ index.js
@@ -0,0 +1,69 @@
+// @flow
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import {requireNativeComponent, ViewPropTypes, processColor} from 'react-native';
+
+class Gradient extends React.Component<NativeProps> {
+  static propTypes = {
+    type: PropTypes.string.isRequired,
+    colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    locations: PropTypes.arrayOf(PropTypes.number),
+    radialRadius: PropTypes.number,
+    radialCenter: PropTypes.arrayOf(PropTypes.number),
+    ...ViewPropTypes, // Include default react view props
+  };
+
+  static defaultProps = {
+    locations: [0.0, 1.0],
+  };
+
+  render() {
+    const colors = this.props.colors.map(processColor);
+    return <NativeView {...this.props} colors={colors} />;
+  }
+}
+
+type GradientType =
+  | 'linear'
+  | 'radial'
+  ;
+
+type NativeProps = {
+  type: GradientType,
+  colors: string[],
+  locations?: ?number[], // iOS only
+  radialRadius?: ?number,
+  radialCenter?: ?[number, number],
+};
+
+type LinearProps = {
+  colors: $PropertyType<NativeProps, 'colors'>,
+  locations?: $PropertyType<NativeProps, 'locations'>, // iOS only
+};
+
+type RadialProps = {
+  colors: $PropertyType<NativeProps, 'colors'>,
+  locations?: $PropertyType<NativeProps, 'locations'>, // iOS only
+  radius: number, // Not optional in case of a radial
+  center?: $PropertyType<NativeProps, 'radialCenter'>,
+};
+
+const NativeView = requireNativeComponent('RNQ42GradientView', Gradient);
+
+export default {
+  Linear: (props: LinearProps) => (
+    <Gradient
+      {...props}
+      type="linear"
+    />
+  ),
+  Radial: ({radius, center, ...otherProps}: RadialProps) => (
+    <Gradient
+      {...otherProps}
+      type="radial"
+      radialRadius={radius}
+      radialCenter={center || [0.5, 0.5]}
+    />
+  ),
+};
--- js/index.js
+++
@@ -1,6 +0,0 @@
-
-import { NativeModules } from 'react-native';
-
-const { Q42RNGradient } = NativeModules;
-
-export default Q42RNGradient;
